c98a8aa01103c0530b5d496dd9022de4bfc7fd46
ktr03282 <zf7heatpb@gmail.com>
2018-05-30 19:40:37 +0900

    feat: get GitHub repositories
    要: storeを使うように修正

--- pages/index.vue
+++ pages/index.vue
@@ -9,9 +9,34 @@
 </template>
 
 <script>
+import axios from "axios"
 
 export default {
   components: {
+  },
+  mounted: async function() {
+    const repos = await this.getGitHubRepositories()
+    console.log(repos)
+  },
+  methods: {
+    getGitHubRepositories() {
+      return new Promise((resolve, reject) => {
+        axios.get("https://api.github.com/users/ktr03282/repos")
+          .then(res => {
+            const repositories = res.data.map(e => {
+              return {
+                name: e.name,
+                html_url: e.html_url,
+                languages_url: e.languages_url,
+                updated_at: e.updated_at,
+                owner: e.owner
+              }
+            })
+            resolve(repositories)
+          })
+          .catch(err => reject(err))
+      })
+    }
   }
 }
 </script>
